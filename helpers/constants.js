4bf053fd21afd84cde47e27c5d20cbc73d805b4d
ChapalBarua <chapalbuet@yahoo.com>
2024-01-19 22:27:17 -0500

    [temple-55] introducing user_role constant and ignoring user_role sent from UI - dont let anyone set user role to admin from outside

--- helpers/constants.js
+++ helpers/constants.js
@@ -3,5 +3,10 @@ module.exports = Object.freeze({
     UPLOADED_IMAGE_PATH: 'C:/Users/chapa/Desktop/temple_uploaded/temple_uploaded_images/',
     SERVED_DIRECTORY: 'C:/Users/chapa/Desktop/temple_uploaded',
     DOWNLOADABLE_FILE_URL: 'https://localhost:3000/temple_uploaded_files/',
-    DOWNLOADABLE_IMAGE_URL: 'https://localhost:3000/temple_uploaded_images/'
+    DOWNLOADABLE_IMAGE_URL: 'https://localhost:3000/temple_uploaded_images/',
+    USER_ROLE: Object.freeze({ 
+        ADMIN: 'Admin',
+        SUPER_ADMIN: 'Super Admin',
+        GENERAL_USER: 'General User'
+    })
 });
