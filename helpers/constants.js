c0461c108996867d93c24edaea7c700de6b0dcff
ChapalBarua <chapalbuet@yahoo.com>
2024-02-03 20:38:00 -0500

    [temple-56] Hide secret keys

--- helpers/constants.js
+++ helpers/constants.js
@@ -2,6 +2,7 @@ module.exports = Object.freeze({
     UPLOADED_FILE_PATH: '/home/ec2-user/temple_uploaded/temple_uploaded_files/',
     UPLOADED_IMAGE_PATH: '/home/ec2-user/temple_uploaded/temple_uploaded_images/',
     SERVED_DIRECTORY: '/home/ec2-user/temple_uploaded',
+    PRIVATE_KEY_PATH: '/home/ec2-user/secrets/private_key.txt',
     DOWNLOADABLE_FILE_URL: 'https://www.bso-toronto.ca:3000/temple_uploaded_files/',
     DOWNLOADABLE_IMAGE_URL: 'https://www.bso-toronto.ca:3000/temple_uploaded_images/',
     USER_ROLE: Object.freeze({ 
