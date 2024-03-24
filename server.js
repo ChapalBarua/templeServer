045c14dbb9bb5ecc3832d23f26a9db2dd4cd187c
ChapalBarua <chapalbuet@yahoo.com>
2024-03-24 19:59:09 -0400

    [temple-57] Donate page, download without navigating, CORS issue resolved

--- server.js
+++ server.js
@@ -18,10 +18,10 @@ function logger(req, res, next){
     console.log(`[${Date.now()}] ${req.method} ${req.url}`);
     next();
 }
+app.use(cors());
 app.use(bodyParser.urlencoded());
 app.use(express.static(constants.SERVED_DIRECTORY));
 app.use(bodyParser.json());
-app.use(cors());
 app.use(logger);
 app.use('/api',appRoutes);
 
